e4421f5588cf065385a5e2e4d92184234d232a04
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-04-21 19:01:47 -0400

    trying to add sphinx documentation...

--- src/shape_model.cpp
+++ src/shape_model.cpp
@@ -8,7 +8,9 @@
 
 #if USING_PYBIND11
 void init_shape_model(py::module & m) {
-  py::class_<Flux::ShapeModel>(m, "ShapeModel")
+  py::class_<Flux::ShapeModel>(m, "ShapeModel", R"delim(
+Testing testing
+)delim")
     .def(py::init<Flux::verts_ref_t, Flux::faces_ref_t>())
     .def_readonly("verts", &Flux::ShapeModel::verts)
     .def_readonly("faces", &Flux::ShapeModel::faces)
